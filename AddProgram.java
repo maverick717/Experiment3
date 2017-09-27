9f593d11bc62501f34a000fbba3a76caf2495a99
Tarun Reddy <tarunreddy@Taruns-MacBook-Air.local>
2017-09-26 21:35:44 -0700

    Initial commit

---
+++ AddProgram.java
@@ -0,0 +1,16 @@
+package DeltaPro;
+
+public class AddProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		int c= a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
---
+++ twoNumbersGreater.java
@@ -0,0 +1,18 @@
+package DeltaPro;
+
+public class twoNumbersGreater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x= 20;
+		int y = 30;
+		if (x>y){
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+	}
+
+}
